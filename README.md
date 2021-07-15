b8b5a20697c06ef39e4603addff59f7317384193
Manny Alfaro <78244552+Mannyalfa@users.noreply.github.com>
2021-07-15 16:37:38 -0400

    initial push

---
+++ README.md
@@ -0,0 +1,51 @@
+# Calculator
+
+# Table of Contents
+
+1. [Description](#description)
+
+2. [Installation](#installation)
+
+3. [Usage](#usage)
+
+4. [License](#license)
+
+5. [Image](#Image)
+
+6. [Languages/Applications](#languages-applications)
+
+7. [Repository URL](#repository-url)
+
+8. [Website](#website)
+
+9. [Credits](#credits)
+
+## Description
+Simple function calculator
+
+## Installation
+None required
+
+## Usage
+Just use like any other calculator
+
+## License
+MIT [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Image
+![screenshot]()
+
+## Languages/Applications
+HTML,CSS,JavaScript
+
+## Repository URL
+https://github.com/Mannyalfa/calculator
+
+## Website
+https://mannyalfa.github.io/calculator/
+   
+    
+## Credits
+Manny Alfaro
+
+### If you have any additional questions regarding this project, please contact me at: mannyalfa@gmail.com
---
+++ assets/js/script.js
@@ -0,0 +1,79 @@
+// get keys from document
+var keys = document.querySelectorAll('#calculator span');
+var operators = ['+', '-', 'x', '÷'];
+var decimalAdded = false;
+ 
+// onclick event to all the keys and perform operations
+for(var i = 0; i < keys.length; i++) {
+	keys[i].onclick = function(e) {
+		// Get the input and button values
+		var input = document.querySelector('.screen');
+		var inputVal = input.innerHTML;
+		var btnVal = this.innerHTML;
+		
+		// append the key values (btnValue) 
+		// If clear key is pressed, erase everything
+		if(btnVal == 'C') {
+			input.innerHTML = '';
+			decimalAdded = false;
+		}
+		
+		// If eval key is pressed, calculate and display the result
+		else if(btnVal == '=') {
+			var equation = inputVal;
+			var lastChar = equation[equation.length - 1];
+			
+			// Replace all instances of x and ÷ with * and / respectively.
+			equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
+			
+			// checking the last character of the equation. If it's an operator or a decimal, remove it
+			if(operators.indexOf(lastChar) > -1 || lastChar == '.')
+				equation = equation.replace(/.$/, '');
+			
+			if(equation)
+				input.innerHTML = eval(equation);
+				
+			decimalAdded = false;
+		}
+
+		
+		// indexOf works only in IE9+
+		else if(operators.indexOf(btnVal) > -1) {
+			// Get the last character from the equation
+			var lastChar = inputVal[inputVal.length - 1];
+			
+			// add operator if input is not empty and there is no operator at the last
+			if(inputVal != '' && operators.indexOf(lastChar) == -1) 
+				input.innerHTML += btnVal;
+			
+			// Allow minus if the string is empty
+			else if(inputVal == '' && btnVal == '-') 
+				input.innerHTML += btnVal;
+			
+			// Replace the last operator (if exists) with the newly pressed operator
+			if(operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
+				input.innerHTML = inputVal.replace(/.$/, btnVal);
+			}
+			
+			decimalAdded =false;
+		}
+		
+		//  flag 'decimalAdded'
+		else if(btnVal == '.') {
+			if(!decimalAdded) {
+				input.innerHTML += btnVal;
+				decimalAdded = true;
+			}
+		}
+		
+		else {
+			input.innerHTML += btnVal;
+		}
+		
+		
+		e.preventDefault();
+	} 
+}
+
+
+
